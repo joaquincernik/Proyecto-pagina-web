31c7bef1ed566ace2e6cc3ba98a519b7aa0dfe4e
joaquincernik <joaquincernik@gmail.com>
2023-03-17 17:30:53 -0300

    arreglando el git ignore

--- controller/mainController.js
+++ controller/mainController.js
@@ -1,18 +1,42 @@
 let db=require("../database/models")
 const mainController={
     index:(req,res)=>{
-        
-        db.Albums.findAll({
+       //si no esta logeado
+        if(!res.locals.isLogged){
+            db.Albums.findAll({
+                order:[
+                    ['date','DESC']
+                ],
+                limit:8
+            })
+            .then(function(albums){
+                let orders=[];
+                return res.render("index",{albums,orders})
+            }).catch(err=>{console.log(err)})}
+    
+            
+          else{
+              let albumsRequest=db.Albums.findAll({
             order:[
                 ['date','DESC']
             ],
             limit:8
         })
-        .then(function(albums){
-            
-            return res.render("index",{albums})
-        }).catch(err=>{console.log(err)})}
+        let orderRequest=db.Orders.findAll({
+           where:{
+            email:res.locals.userLogged.email
+           }})
 
+           Promise.all([albumsRequest,orderRequest])
+           .then(function([albums,orders]){
+            
+            return res.render("index",{albums:albums,orders:orders})
+           })}  
+          
+            
+           
+        }
+       
        // res.render("index");
     
 }
--- routes/ordersRouter.js
+++ routes/ordersRouter.js
@@ -1,12 +1,14 @@
 const express=require('express')
 const router=express.Router();
 const orderController=require('../controller/orderController');
-const orderMiddleware=require("../middlewares/orderMiddleware")
+const orderMiddleware=require("../middlewares/orderMiddleware");
+const isAdminRoutesMiddleware=require("../middlewares/isAdminRoutesMiddleware")
+
 
 router.get('/create/:id',orderMiddleware,orderController.create)
 router.post('/create/:id',orderController.createProcess)
 
-router.get('/list',orderController.listOrders)
+router.get('/list',isAdminRoutesMiddleware,orderController.listOrders)
 
 router.post('/delete/:id',orderController.delete)
 module.exports=router
